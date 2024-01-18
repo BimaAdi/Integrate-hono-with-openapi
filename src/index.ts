33d2d69a04eb61a5bd28159cdc7f0828c6ad6000
BimaAdi <bimaadi419@gmail.com>
2024-01-18 08:45:14 +0700

    setup openapi and swagger

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { serve } from "@hono/node-server";
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
+
+const app = new OpenAPIHono();
+
+// The openapi.json will be available at /doc
+app.doc("/doc", {
+	openapi: "3.0.0",
+	info: {
+		version: "1.0.0",
+		title: "My API",
+	},
+});
+
+// swagger ui doc will be available at {server url}/ui
+// fell free to change the url
+// swaggerUI url must have same path as openapi.json
+app.get("/ui", swaggerUI({ url: "/doc" }));
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+	fetch: app.fetch,
+	port,
+});
